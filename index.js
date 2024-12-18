9ed69388c77f2eec527af6daae73f4fe10c69ceb
MPrasad1611 <prasadchowdary1611@gmail.com>
2024-12-18 15:03:03 +0530

    jsvar

---
+++ index.js
@@ -0,0 +1,18 @@
+// var a;
+// a=10;
+// var a;
+// a=110;
+// console.log(a);
+// let x=10;
+// x=x+10
+// console.log(x);
+// var r=10;
+// let x=12345;
+// console.log(x);
+// let x=[1,2,true,'hi']
+// console.log(x.length);
+let x={key1:2,key2:"hello",key3:true};
+console.log(x);
+console.log(typeof x);
+console.log(x.key2);
+
