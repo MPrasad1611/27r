45dc40be98cd18735a91524248c55f4968a573c4
MPrasad1611 <prasadchowdary1611@gmail.com>
2024-12-24 18:24:11 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-for(let i=1;i<=50;i++){
-    console.log(`5 X ${i} = ${5*i} `)
+// for(let i=1;i<=50;i++){
+//     console.log(`5 X ${i} = ${5*i} `)
+// }
+// let str="Welcome"
+// let s="";
+// for(i=str.length-1;i>=0;i--){
+//     s+=str[i]
+  
+// };
+// console.log(s);
+// let str="Welcome"
+let s="";
+for(let i=0;i<5;i++)
+{
+    s+="*";
+    console.log(s);
 }
