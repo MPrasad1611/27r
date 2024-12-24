583141d94535ed4e54e573bd1bd7298f76232b26
MPrasad1611 <prasadchowdary1611@gmail.com>
2024-12-24 17:05:38 +0530

    changes

--- index.js
+++ index.js
@@ -15,49 +15,49 @@
 //   ? console.log("Chicken")
 //   : console.log("Mutton");
 
-let isHtml = true;
-let isCss = true;
-let isboot = true;
-let isJs = true;
-let isReact = true;
-let isNode = true;
-console.log("OUTPUT:-")
-console.log("--------------------")
-isHtml &&
-isCss &&
-isboot &&
-isJs == false &&
-isReact == false &&
-isNode == false
-  ? console.log("Then Become UI/UX Developer")
-  : isHtml && isCss && isboot && isJs && isReact && isNode == false
-  ? console.log("Then Become Frontend Developer")
-  : isHtml && isCss && isboot && isJs && isReact == false && isNode
-  ? console.log("Then Become Backend Developer")
-  : isHtml && isCss && isboot && isJs && isReact && isNode
-  ? console.log("Then Become FullStack Developer")
-  : console.log("GO and Join 10000Coders");
+// let isHtml = true;
+// let isCss = true;
+// let isboot = true;
+// let isJs = true;
+// let isReact = true;
+// let isNode = true;
+// console.log("OUTPUT:-")
+// console.log("--------------------")
+// isHtml &&
+// isCss &&
+// isboot &&
+// isJs == false &&
+// isReact == false &&
+// isNode == false
+//   ? console.log("Then Become UI/UX Developer")
+//   : isHtml && isCss && isboot && isJs && isReact && isNode == false
+//   ? console.log("Then Become Frontend Developer")
+//   : isHtml && isCss && isboot && isJs && isReact == false && isNode
+//   ? console.log("Then Become Backend Developer")
+//   : isHtml && isCss && isboot && isJs && isReact && isNode
+//   ? console.log("Then Become FullStack Developer")
+//   : console.log("GO and Join 10000Coders");
 // let str="somethings"
 // for(let i in str){
 //   console.log("Hello",`2X${i}=${i*2}`)
 // }
-// let students = [
-//   { id: 1, s_name: "Pavan", city: "Hyd" },
-//   { id: 2, s_name: "Arjun", city: "Sec" },
-//   { id: 3, s_name: "Meena", city: "Vizag" },
-//   { id: 4, s_name: "Rahul", city: "Bengaluru" },
-//   { id: 5, s_name: "Sita", city: "Chennai" },
-//   { id: 6, s_name: "Vijay", city: "Mumbai" }
-//   ];
-//   console.log("--------------------")
-//   console.log("In FOR OF LOOP")
-//   console.log("--------------------")
-//   for(let i of students){
-//     console.log(`${i.s_name} from ${i.city} `)
-//   }
-//   console.log("--------------------")
-//   console.log("In FOR IN LOOP")
-//   console.log("--------------------")
-//   for(let i in students){
-//     console.log(`${students[i].s_name} from ${students[i].city} `)
-//   }
+let students = [
+  { id: 1, s_name: "Pavan", city: "Hyd" },
+  { id: 2, s_name: "Arjun", city: "Sec" },
+  { id: 3, s_name: "Meena", city: "Vizag" },
+  { id: 4, s_name: "Rahul", city: "Bengaluru" },
+  { id: 5, s_name: "Sita", city: "Chennai" },
+  { id: 6, s_name: "Vijay", city: "Mumbai" }
+  ];
+  console.log("--------------------")
+  console.log("In FOR OF LOOP")
+  console.log("--------------------")
+  for(let i of students){
+    console.log(`${i.s_name} from ${i.city} `)
+  }
+  console.log("--------------------")
+  console.log("In FOR IN LOOP")
+  console.log("--------------------")
+  for(let i in students){
+    console.log(`${students[i].s_name} from ${students[i].city} `)
+  }
