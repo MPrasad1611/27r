04237dcd659f11b2eb8808a7b3ec7fcf25d40426
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-02 18:03:12 +0530

    changes

---
+++ index.js
@@ -0,0 +1,6 @@
+ //    let data=fetch("https://679f552e24322f8329c360ab.mockapi.io/movies")
+      //    console.log(data)
+      //    data.then((x)=>x.json()).then((x)=>{
+      //     let r=x;
+      //     r.map((x)=>{console.log(x)})
+      //    }).catch()
