7511b40a12421537bf0d7bbb7f8ad971ed7cbcb1
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-04 20:52:21 +0530

    changes

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-function delay(){
-    return new Promise((r,rj)=>{
-        setTimeout(()=>{
-        if(true){
-            r('done');
-        }
-        else{
-            rj('error');
-        }
-    },2000);
-    });
+// function delay(){
+//     return new Promise((r,rj)=>{
+//         setTimeout(()=>{
+//         if(true){
+//             r('done');
+//         }
+//         else{
+//             rj('error');
+//         }
+//     },2000);
+//     });
+// }
+// delay().then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+// let arr=[1,2,3,4,5];
+// // [a,b,c,d,e]=arr;
+// // console.log(a,b,c,d,e);
+// [a,b,...c]=arr;
+// console.log(c);
+let obj={
+    name:'sachin',id:101,age:25
 }
-delay().then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    console.log(err);
-});
+const {name,id,...age}=obj;
+console.log(name,id,age);
